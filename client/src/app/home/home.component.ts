627b609be9a8372baf192dcab9bed71781b5934f
agathesraj <agathesraj@gmail.com>
2025-07-02 20:07:00 +0530

    Added JWT Interceptors, Member card and Member list along with Photos gallery integration

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,26 +8,14 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
-cancelRegisterMode(event: boolean) {
- this.registerMode=event;
-}
-  http = inject(HttpClient);
+export class HomeComponent{
   registerMode = false; 
-  users: any;
+  
   registerToggle(){
     this.registerMode=!this.registerMode;
   }
 
-    ngOnInit(): void {
-    this.getUsers();
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request completed')
-    });
+  cancelRegisterMode(event: boolean) {
+ this.registerMode=event;
 }
 }
