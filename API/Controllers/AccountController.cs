5fa45aedcfb870d9c2335fccb355ec83dc4a1b26
agathesraj <agathesraj@gmail.com>
2025-07-09 14:04:26 +0530

    Migrated to Reactive Forms and added few things related to user registration

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,12 +5,13 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+    public class AccountController(DataContext context, ITokenService tokenService, IMapper mapper) : BaseApiController
     {
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
@@ -19,21 +20,27 @@ namespace API.Controllers
             {
                 return BadRequest("Username is taken");
             }
-            return Ok();
-            // using var hmac = new HMACSHA512();
+            using var hmac = new HMACSHA512();
+
+            var user = mapper.Map<AppUser>(registerDto);
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
+
             // var user = new AppUser
             // {
             //     UserName = registerDto.Username.ToLower(),
             //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             //     PasswordSalt = hmac.Key
             // };
-            // context.Users.Add(user);
-            // await context.SaveChangesAsync();
-            // return new UserDto
-            // {
-            //     Username = user.UserName,
-            //     Token = tokenService.CreateToken(user)
-            // };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
+            };
         }
 
         [HttpPost("login")]
@@ -54,6 +61,7 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
+                KnownAs = user.KnownAs,
                 Token = tokenService.CreateToken(user),
                 Photourl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url
             };            
